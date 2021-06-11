23a89b6f06dcd0535636db85b0782e1bce409c11
LGXfufile <401040990@qq.com>
2021-06-12 01:16:21 +0800

    Initial commit jenkins

---
+++ src/main/java/com/xin/controller/myController.java
@@ -0,0 +1,16 @@
+package com.xin.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "hello";
+
+    }
+}
